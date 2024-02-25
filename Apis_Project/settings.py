7822aa06096c06c42a66717349ead1a78ed1270c
Leduan Rosell <leduanb@gmail.com>
2024-02-25 00:21:39 -0600

    Added package djangorestframework-camel-case to automatically convert from snake_case to camelCase and vice versa

--- Apis_Project/settings.py
+++ Apis_Project/settings.py
@@ -131,6 +131,13 @@ REST_FRAMEWORK = {
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         "rest_framework.authentication.SessionAuthentication"
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer'
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser'
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'NON_FIELD_ERRORS_KEY': 'general_errors',
